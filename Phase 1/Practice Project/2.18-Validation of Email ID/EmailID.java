43aaa22c8db872698d610b63a69c73f8a2b82e38
Vignesh Raaj <nsvigneshraaj@gmail.com>
2022-03-07 02:28:54 +0530

    Made some enhancements in the code

--- Phase 1/Practice Project/2.18-Validation of Email ID/EmailID.java
+++ Phase 1/Practice Project/2.18-Validation of Email ID/EmailID.java
@@ -33,22 +33,41 @@ public class EmailID {
 
             boolean b= EmailID.validateEmail(s);
             if(b==true){
-                i = emailList.indexOf(s);
-                System.out.println("The given Email ID "+s+" is present in "+(i+1)+"th position in the system");                
-                break;
+                if(emailList.contains(s)){
+                    i = emailList.indexOf(s);
+                    System.out.println("\nThe given Email ID "+s+" is present in "+(i+1)+"th position in the system");
+                }
+
+                else{
+                    System.out.println("\nThe given Email ID "+s+" is not present in the system");
+                }
+
+                System.out.println("\nEnter 'Yes' if you want to see all the Email IDs present in the system or 'Exit' to exit");
+                String op= sc.next();
+
+                if(op.equalsIgnoreCase("Yes")){
+                    ListIterator<String> itr= emailList.listIterator();
+                    while(itr.hasNext()){
+                        System.out.println(itr.next());
+                    }
+                    sc.close();
+                    System.exit(0);
+                }
+
+                else{
+                    System.exit(0);
+                }
             }
 
             else{
-                System.out.println("Enter a valid Email ID to continue the search or enter 'exit' to exit the program\n");
+                System.out.println("\nEnter a valid Email ID to continue the search or enter 'exit' to exit the program\n");
                 continue;
             }
         }
-        sc.close();
-        System.exit(0);
     }
 
     public static boolean validateEmail(String email){
-        String regex = "^[A-Za-z0-9+_.-]+@(.+)$";
+        String regex = "^[A-Za-z0-9+_.-]+@+\\binfymax.com\\b$";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(email);
         return matcher.matches();
